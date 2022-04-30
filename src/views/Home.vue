20e43f7775f73652056a84d702fbbf807042e22e
hao <2524129018@qq.com>
2022-04-30 14:40:38 +0800

    初始化 aside 的 html

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,7 +6,7 @@
       <h2>一个很厉害的UI框架</h2>
       <p class="actions">
         <a href="">Github</a>
-        <a href="">开始</a>
+        <router-link to="/doc">开始</router-link>
       </p>
     </div>
   </div>
@@ -20,20 +20,24 @@ export default {
 }
 </script>
 <style lang="scss" scoped>
-.topnav {
-  background: pink;
+.banner {
+  padding: 100px 0;
   display: flex;
-  padding: 16px;
-  > .logo {
-    max-width: 6em;
-    margin-right: auto;
-  }
-  > .menu {
-    display: flex;
-    white-space: nowrap;
-    flex-wrap: nowrap;
-    > li {
-      margin: 0 1em;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  background: lightgreen;
+  > .actions {
+    padding: 8px 0;
+    a {
+      margin: 0 8px;
+      background: #fff;
+      display: inline-block;
+      $h: 28px;
+      height: $h;
+      line-height: $h;
+      border-radius: $h/2;
+      padding: 0 8px;
     }
   }
 }
