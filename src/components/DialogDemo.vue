6a7f7a851624d62c8447c1f9938775432f2590d6
hao <2524129018@qq.com>
2022-05-10 15:28:46 +0800

    闯将dialog组件

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,3 +1,14 @@
 <template>
-  <div>dialog示例</div>
+  <h1>示例1</h1>
+  <Dialog></Dialog>
 </template>
+
+<script lang="ts">
+import Dialog from '../lib/Dialog.vue'
+export default {
+  components: {
+    Dialog,
+  },
+}
+</script>
+<style scoped></style>
